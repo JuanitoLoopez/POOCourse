635d8263d70a63c7f9c4a27d9fcd8a936de1f6e2
juanitoloopez <juanchito15810@gmail.com>
2021-12-14 20:34:11 -0600

    Working with python. Creating the constructor and more things.

--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,17 @@
+from account import Account
 from car import Car 
 
 if __name__ == "__main__":
     print("Hola Mundo")
 
-    car = Car()
-    car.license = "XAN321"
-    car.driver = "Andres Herrera"
+    car = Car("AMX323", Account("Marcos Cruz", "ASGF3245"))
     print(vars(car))
+    print(vars(car.driver))
+    #car.license = "XAN321"
+    #car.driver = "Andres Herrera"
 
-    car2 = Car()
-    car2.license = "ZYZ323"
-    car2.driver = "Maria Herrera"
+    car2 = Car("XAW324", Account("Mario Gonzalez", "234BASD"))
     print(vars(car2))
+    print(vars(car2.driver))
+    #car2.license = "ZYZ323"
+    #car2.driver = "Maria Herrera"
